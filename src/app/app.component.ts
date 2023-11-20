e138268fd8df42a29fe4528e3ab1cfda7c9749d8
Santosh Dhakal <santosh.dhakal07@gmail.com>
2023-11-20 09:58:54 -0500

    make drawer responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
 import { NavDrawerComponent } from './nav-drawer/nav-drawer.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ResumeService } from './services/resume.service';
+import { MatIconModule } from '@angular/material/icon';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +18,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     RouterLinkActive,
     MatSidenavModule,
     NavDrawerComponent,
+    MatIconModule,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
@@ -22,4 +26,28 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 export class AppComponent {
   title = 'ng-my-perfect-resume';
   opened = true;
+  showMenu = false;
+  mode = new FormControl('side' as MatDrawerMode);
+
+  constructor(public resumeService: ResumeService) {
+    const mediaQuery = window.matchMedia('screen and (min-width: 1024px)');
+    const changeListener = (e: any) => {
+      if (e.matches) {
+        this.opened = true;
+        this.showMenu = false;
+        this.mode = new FormControl('side' as MatDrawerMode);
+      } else {
+        this.opened = false;
+        this.showMenu = true;
+        this.mode = new FormControl('over' as MatDrawerMode);
+      }
+    };
+    changeListener(mediaQuery);
+    if (mediaQuery.addEventListener) {
+      mediaQuery.addEventListener('change', changeListener);
+    } else {
+      //for backward compatibility with older safari broswers
+      mediaQuery.addListener(changeListener);
+    }
+  }
 }
